8e8ce791c103b6fa934eecba7823e4081ed06e97
jalen lane <bub7684@gmail.com>
2020-01-26 20:55:39 -0500

    Add project files.

---
+++ Fighter_Game_Metadata.cs
@@ -0,0 +1,68 @@
+﻿namespace Fighter_Game_Metadata
+{
+
+    class GlobalVariables
+    {
+        bool PlayerOneTurn;
+        bool PlayerTwoTurn;
+        string StartDecision;
+        string Tutorial;
+        int GameRound;
+
+    }
+
+    class FighterClass
+    {
+        public string name;
+        public int health;
+        public int armor;
+        public double speed;
+        public int power;
+        public int stamina;
+    }
+
+    class Knight : FighterClass
+    {
+        public Knight()
+        {
+            name = "Knight";
+            health = 250;
+            armor = 25;
+            speed = .75;
+            power = 25;
+            stamina = 100;
+        }
+
+        public string KnightDescription()
+        {
+            return "A well rounded fighter. baseline stats across the board, and 250 health.";
+        }
+
+        public string KnightAbilities()
+        {
+            return "Placeholder";
+        }
+    }
+
+    class Tank : FighterClass
+    {
+        public Tank()
+        {
+            name = "Knight";
+            health = 250;
+            armor = 25;
+            speed = .75;
+            power = 25;
+            stamina = 100;
+        }
+        public string TankDescription()
+        {
+            return "A bulky fighter. Has additional health and armor, and a little more power. Has decreased speed however.";
+        }
+
+        public string TankAbilities()
+        {
+            return "Placeholder";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fighter_Game_Metadata;
+namespace Fighter_Game__C_Sharp_Edition_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight player = new Knight();
+            Console.WriteLine(player.name);
+        }
+    }
+}
